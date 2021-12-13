2d370f25e83fd3dfc916ba8602330c2b1940161c
Dillon Estrada <dillon.estrada55@gmail.com>
2021-12-13 12:51:41 -0600

    Add project files.

---
+++ BillManager/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BillManager.Models;
+
+namespace BillManager.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //var billsList = new List<BillEntry>();
+
+            //BillEntry newBill = new BillEntry
+            //{
+            //    Name = "Test",
+            //    Amount = 10.45,
+            //    DueDate = new DateTime(2021, 12, 25)
+            //};
+
+            //billsList.Add(newBill);
+
+            var bill = new BillEntry();
+
+
+            return View(bill);
+        }
+
+        [HttpPost]
+        public ActionResult Index(BillEntry entry)
+        {
+            BillEntry.addBill(entry.Name, entry.Amount, entry.DueDate);
+
+            return View();
+        }
+    }
+}
---
+++ BillManager/Models/BillEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Web;
+
+namespace BillManager.Models
+{
+    public class BillEntry
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Amount is required")]
+        public double Amount { get; set; }
+        [Required(ErrorMessage = "Due Date is required")]
+        [DataType(DataType.Date)]
+        public DateTime DueDate { get; set; }
+
+        public static void addBill(string Name, double Amount, DateTime DueDate)
+        {
+
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source =" + HttpContext.Current.Server.MapPath("~/") + "DB/Bills.db; Version = 3; New = false;"))
+            {
+                using (SQLiteCommand cmd = new SQLiteCommand())
+                {
+                    string strSql = String.Format("INSERT INTO [Bills] ([Name],[Amount],[DueDate]) VALUES ('{0}', {1}, '{2}')", Name, Amount, DueDate);
+                    cmd.CommandText = strSql;
+                    cmd.Connection = conn;
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    conn.Close();
+                }
+            }
+        }
+    }
+}
